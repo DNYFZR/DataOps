8fa53451f5d09d5536952f07f2571e0e35d01a85
Daniel <76977155+iDataEngineer@users.noreply.github.com>
2022-07-02 15:49:44 +0100

    Updated ATP.sql

--- dbt_project/models/ATP.sql
+++ dbt_project/models/ATP.sql
@@ -3,6 +3,6 @@
 
 SELECT * 
   FROM matches
- WHERE winner_name = 'Rafael Nadal' AND loser_name = 'Novak Djokovic' 
-       OR winner_name = 'Novak Djokovic' AND loser_name = 'Rafael Nadal'
-       AND tourney_level = 'G'
+ WHERE tourney_level = 'G' 
+       AND (winner_name = 'Rafael Nadal' AND loser_name = 'Novak Djokovic' 
+       OR winner_name = 'Novak Djokovic' AND loser_name = 'Rafael Nadal')
